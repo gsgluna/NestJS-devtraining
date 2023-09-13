70186cd0858ee07e0ad074870e302b9785106e8d
Samuel Luna <92549469+gsgluna@users.noreply.github.com>
2023-09-13 12:19:46 -0300

    Update customer.entity.ts

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity('customers')
 export class Customer {
     @PrimaryGeneratedColumn()
-    id: string;
+    id: number;
 
     @Column()
     username: string;
